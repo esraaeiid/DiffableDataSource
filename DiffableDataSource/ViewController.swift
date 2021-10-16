e756fe14b1c2fed4e6cb155ddca30e9df116592b
Esraa Eid <esraa@ryets.com>
2021-10-15 20:37:31 -0700

    added sections header supplementary  view

--- DiffableDataSource/ViewController.swift
+++ DiffableDataSource/ViewController.swift
@@ -48,6 +48,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         setupCollectionView()
         configureSearchController()
         applySnapshot(animatingDifferences: false)
+        configureLayout()
     }
     
     override func viewDidLayoutSubviews() {
@@ -70,6 +71,25 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                                         image: article.thumbnail ?? nil))
           return cell
       })
+        
+        
+        // 1
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+          // 2
+          guard kind == UICollectionView.elementKindSectionHeader else {
+            return nil
+          }
+          // 3
+          let view = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: SectionHeaderReusableView.reuseIdentifier,
+            for: indexPath) as? SectionHeaderReusableView
+          // 4
+          let section = self.dataSource.snapshot()
+            .sectionIdentifiers[indexPath.section]
+          view?.titleLabel.text = section.title
+          return view
+        }
       return dataSource
     }
     
@@ -124,6 +144,53 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return CGSize(width: view.frame.width, height: 150)
     }
     
+}
+
+// MARK: - Layout Handling
+extension ViewController {
+  private func configureLayout() {
+    collectionView.register(
+      SectionHeaderReusableView.self,
+      forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+      withReuseIdentifier: SectionHeaderReusableView.reuseIdentifier
+    )
+    
+    collectionView.collectionViewLayout = UICollectionViewCompositionalLayout(sectionProvider: { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+      let isPhone = layoutEnvironment.traitCollection.userInterfaceIdiom == UIUserInterfaceIdiom.phone
+      let size = NSCollectionLayoutSize(
+        widthDimension: NSCollectionLayoutDimension.fractionalWidth(1),
+        heightDimension: NSCollectionLayoutDimension.absolute(isPhone ? 280 : 250)
+      )
+      let itemCount = isPhone ? 1 : 3
+      let item = NSCollectionLayoutItem(layoutSize: size)
+      let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitem: item, count: itemCount)
+      let section = NSCollectionLayoutSection(group: group)
+      section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+      section.interGroupSpacing = 10
+        
+        
+        // Supplementary header view setup
+        let headerFooterSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(20)
+        )
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerFooterSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        section.boundarySupplementaryItems = [sectionHeader]
+        
+      return section
+    })
+  }
+  
+  override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    super.viewWillTransition(to: size, with: coordinator)
+    coordinator.animate(alongsideTransition: { context in
+      self.collectionView.collectionViewLayout.invalidateLayout()
+    }, completion: nil)
+  }
 }
 
 // MARK: - UISearchResultsUpdating Delegate
